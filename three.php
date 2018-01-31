e464291790ed77c73bf42dd317e03b1d6848bb46
Ma. Jackie Lyn Sayson <34363011+kiean0918@users.noreply.github.com>
2018-01-30 20:16:21 -0800

    Add files via upload

---
+++ three.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<a href="three.php"><button type="button">next</button></a>
+	<a href="four.php"><button type="button">next</button></a>
+	  <img src="3.jpg">
+	
+
+</body>
+</html>
---
+++ two.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<a href="index.php"><button type="button">next</button></a>
+	<a href="three.php"><button type="button">next</button></a>
+	  <img src="2.jpg">
+	
+
+</body>
+</html>
